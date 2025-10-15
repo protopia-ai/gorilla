20bc9c4230ce36ead7cd835dbf7f73a98012ff07
sidhartha-roy <sid@protopia.ai>
2025-10-15 13:26:42 -0500

    feat: :sparkles: use bfcl to test vllm chat/completions endpoint

--- berkeley-function-call-leaderboard/bfcl_eval/constants/model_config.py
+++ berkeley-function-call-leaderboard/bfcl_eval/constants/model_config.py
@@ -75,7 +75,7 @@ from bfcl_eval.model_handler.local_inference.think_agent import ThinkAgentHandle
 @dataclass
 class ModelConfig:
     """
-    Model configuration class for storing model metadata and settings. 
+    Model configuration class for storing model metadata and settings.
 
     Attributes:
         model_name (str): Name of the model as used in the vendor API or on Hugging Face (may not be unique).
@@ -1242,6 +1242,18 @@ local_inference_model_map = {
         input_price=None,
         output_price=None,
         is_fc_model=True,
+        underscore_to_dot=False,
+    ),
+    "meta-llama/Llama-3.2-1B-Instruct": ModelConfig(
+        model_name="meta-llama/Llama-3.2-1B-Instruct",
+        display_name="Llama-3.2-1B-Instruct (FC)",
+        url="https://llama.meta.com/llama3",
+        org="Meta",
+        license="Meta Llama 3 Community",
+        model_handler=OpenAICompletionsHandler,
+        input_price=None,
+        output_price=None,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "meta-llama/Llama-3.2-3B-Instruct-FC": ModelConfig(
